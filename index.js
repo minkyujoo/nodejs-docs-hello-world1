e242f712147f10b21be76d52d0aa9656403f362e
Minkyu Joo <joo.minkyu@gmail.com>
2021-04-15 17:36:51 +0900

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const http = require('http');
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello1112234435567 IU!");
+    response.end("Hello11122344355678 IU!");
 });
 
 const port = process.env.PORT || 3000;
 //const port = process.env.PORT || 1337;    // for azure devops
 server.listen(port);
 
-console.log("Server112234435657 running at http://localhost:%d", port);
+console.log("Server1122344356578 running at http://localhost:%d", port);
